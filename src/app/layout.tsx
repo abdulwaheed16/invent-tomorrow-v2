cb36b988206b636977ff97fe00c9a8116109ffff
abdulwaheed16 <abwaheed.ahmed@gmail.com>
2025-11-08 01:53:29 +0500

    gtm: google tag manager added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import LoadingScreen from "@/components/loading-screen";
 import Providers from "@/components/providers";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Script from "next/script";
 import { Suspense } from "react";
 import "./globals.css";
 
@@ -41,7 +42,33 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager Script */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MR5983B5');
+          `}
+        </Script>
+      </head>
       <body className={`${segoeUi.className}  antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MR5983B5"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <main className="relative scroll-smooth overflow-x-hidden">
           <Providers>
             <Suspense fallback={<LoadingScreen />}>{children}</Suspense>
